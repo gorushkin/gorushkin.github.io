68736002fe46e215ccbac5cacfc1d967a550c595
gorushkin <gorushkin@users.noreply.github.com>
2020-10-26 20:47:41 +0000

    Deploying to master from  @ c92c611f68d5f0b9b38efd955543a3b548c841e5 🚀

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 (function () {
   const btnList = document.querySelectorAll('.btnlink');
-  const anchor = document.querySelector('.anchor');
-  // var animationTime = 600;
+  const anchor = document.querySelector('.anchor'); // var animationTime = 600;
   // var framesCount = 100;
   // if (anchor) {
   //   var anchorPosition = anchor.getBoundingClientRect().top + pageYOffset;
@@ -12,7 +11,7 @@
   }
 
   function findBtn(i) {
-    btnList[i].addEventListener('click', (e) => {
+    btnList[i].addEventListener('click', e => {
       e.preventDefault();
       ScrollToResolver(anchor);
     });
@@ -22,11 +21,11 @@
     const anchorPosition = anchor.getBoundingClientRect().top + pageYOffset;
     const animationTime = 600;
     const framesCount = 100;
-
     const scrollBy = anchorPosition / framesCount;
     var scroller = setInterval(() => {
       const elem = anchor.getBoundingClientRect().top + pageYOffset;
-      const win = (window.pageYOffset + window.innerHeight);
+      const win = window.pageYOffset + window.innerHeight;
+
       if (scrollBy < elem - win) {
         window.scrollBy(0, scrollBy);
       } else {
@@ -35,4 +34,5 @@
       }
     }, animationTime / framesCount);
   }
-}());
+})();
+//# sourceMappingURL=main.js.map
--- js/vendor.js
+++ js/vendor.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 'use strict';
 
 (function () {
@@ -18,7 +17,6 @@
           }
         });
       });
-
       lazyloadImages.forEach(function (image) {
         imageObserver.observe(image);
       });
@@ -34,11 +32,12 @@
         lazyloadThrottleTimeout = setTimeout(function () {
           var scrollTop = window.pageYOffset;
           lazyloadImages.forEach(function (img) {
-            if (img.offsetTop < (window.innerHeight + scrollTop)) {
+            if (img.offsetTop < window.innerHeight + scrollTop) {
               img.src = img.dataset.src;
               img.classList.remove('lazy');
             }
           });
+
           if (lazyloadImages.length == 0) {
             document.removeEventListener('scroll', lazyload);
             window.removeEventListener('resize', lazyload);
@@ -51,6 +50,5 @@
       window.addEventListener('resize', lazyload);
       window.addEventListener('orientationChange', lazyload);
     }
-  })
+  });
 })();
-
